0cd87dc6cd2f83fd1b499c67b80d1a5b39589c93
darrylemiles <dmilesbacay@gmail.com>
2026-03-25 10:45:36 +0800

    feat: add SWR_OPTIONS and API_BASE_URL constants to manage API configurations

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -29,10 +29,24 @@ const USER_ROLES = {
   },
 };
 
+const SWR_OPTIONS = {
+  revalidateIfStale: true,
+  revalidateOnFocus: true,
+  revalidateOnReconnect: true,
+  onSuccess: (data: any, key: string, config: any) => data
+};
+
+const ENVIRONMENT_MODE = import.meta.env.MODE;
+const API_BASE_URL =
+  ENVIRONMENT_MODE === "PRODUCTION"
+    ? import.meta.env.VITE_API_PROD_URL
+    : import.meta.env.VITE_APP_API_URL_LOCAL;
 
 export {
   APP_NAME,
   APP_EMAIL,
   APP_LOGO,
   USER_ROLES,
+  SWR_OPTIONS,
+  API_BASE_URL
 }
