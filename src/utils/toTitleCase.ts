5202a0462e5a2011135b8ac3ab3b01f83bf0a278
darrylemiles <dmilesbacay@gmail.com>
2026-04-08 11:47:06 +0800

    fix: update user role default value and improve title case utility function

--- src/utils/toTitleCase.ts
+++ src/utils/toTitleCase.ts
@@ -1,7 +1,9 @@
 const toTitleCase = (str: string) => {
-  return str.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase().replace(/_/g, " ")
-  });
+  return str
+    .split(/[\s_]+/)
+    .filter(Boolean)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
 }
 
 export default toTitleCase;
