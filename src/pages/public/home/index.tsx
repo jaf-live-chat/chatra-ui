4947f71c85d7d5dc612ab183560f9a5e0359b7c8
darrylemiles <dmilesbacay@gmail.com>
2026-04-09 17:46:56 +0800

    fix: clean up imports and adjust formatting in Home component

--- src/pages/public/home/index.tsx
+++ src/pages/public/home/index.tsx
@@ -1,22 +1,20 @@
+import { PageTitle } from "../../../components/common/PageTitle";
 import Navbar from "../../../components/common/Navbar";
 import HeroSection from "../../../sections/homepage/HeroSection";
 import TrustedBySection from "../../../sections/homepage/TrustedBySection";
 import ProductsSection from "../../../sections/homepage/ProductsSection";
 import HowItWorksSection from "../../../sections/homepage/HowItWorksSection";
 import DashboardPreviewSection from "../../../sections/dashboard/DashboardPreviewSection";
-import PricingSection from "../../../sections/homepage/PricingSection";
 import IntegrationSection from "../../../sections/homepage/IntegrationSection";
 import CTASection from "../../../sections/homepage/CTASection";
 import Footer from "../../../components/common/Footer";
-import LiveChatWidget from "../../../components/widgets/LiveChatWidget";
 import HomepageFaqSection from "../../../sections/homepage/HomepageFaqSection";
 import React from "react";
-import { PageTitle } from "../../../components/common/PageTitle";
 
 const Home = () => {
   return (
     <React.Fragment>
-       <PageTitle
+      <PageTitle
         title="Home"
         description="JAF Chatra - The Ultimate AI-Powered Customer Support Solution for Your Business"
         canonical="/portal/home"
@@ -27,16 +25,15 @@ const Home = () => {
         <main>
           <HeroSection />
           <TrustedBySection />
-        <ProductsSection />
-        <HowItWorksSection />
-        <DashboardPreviewSection />
-        <IntegrationSection />
-        <HomepageFaqSection />
-        <CTASection />
-      </main>
-      <Footer />
-      <LiveChatWidget />
-    </div>
+          <ProductsSection />
+          <HowItWorksSection />
+          <DashboardPreviewSection />
+          <IntegrationSection />
+          <HomepageFaqSection />
+          <CTASection />
+        </main>
+        <Footer />
+      </div>
     </React.Fragment>
   );
 }
